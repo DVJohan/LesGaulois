467c57aa9cfd1f7ada5c8f03836c94c7d468cc2f
Johan Del valle <dlj4531a@ensfsi.onmicrosoft.com>
2024-02-27 18:46:57 +0100

    Tp1 histoire terminée

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -7,7 +7,9 @@ import personnages.Gaulois;
 import personnages.Romain;
 
 public class Scenario {
+
 	public static void main(String[] args) {
+		
 		Druide panoramix = new Druide("Panoramix",5,10);
 		Romain minus = new Romain("Minus",6);
 		Gaulois obelix = new Gaulois("Obelix", 25);
@@ -21,6 +23,7 @@ public class Scenario {
 		asterix.parler("bonjour");
 		minus.parler("UN GAU... UN GAUGAU...");
 		asterix.frapper(minus);
+		
 	}
 
 }
